71edf3e56cf28ccb5b69d5ecd8d5342e9a85eb48
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-04-08 19:47:09 -1000

    Refresh mix deps and package docs files

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule ChzEx.MixProject do
       {:excoveralls, "~> 0.18", only: :test},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40.0", only: :dev, runtime: false}
     ]
   end
 
